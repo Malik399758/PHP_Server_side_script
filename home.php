eeaef75cd461638c069a23e25661617c53f6a210
Abidcomputers <ym399758@gmail.com>
2025-07-01 11:56:04 -0700

    declared var and print as well.

--- home.php
+++ home.php
@@ -1,7 +1,29 @@
 <?php
 
+// first program of php
+
 echo 'Hello World'.'<br>';
-echo "Welcome to Php programming language";
+echo "Welcome to Php programming language".'<br>';
+
+print('Hello world using print'.'<br>');
+
+printf('Hello world using printf'.'<br>');
+
+
+// user information
+
+// how to declare variable in php using $
+$id = 01;
+$name = 'Yaseen Malik';
+$password = 2211;
+
+
+// print or display on local host 
+
+echo 'User Id : ',$id.'<br>';
+echo 'User Name : ',$name.'<br>';
+echo 'Password :',$password.'<br>'; 
+
 
 
 
